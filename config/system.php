ac7ec8012e8692c01ae685c93d4b3cff840ee42c
Hannah Casier <hannah.casier@me.com>
2025-06-08 18:11:22 +0200

    added secure middleware

---
+++ config/system.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | System Token
+    |--------------------------------------------------------------------------
+    |
+    | This token is used for system-level API access, typically from the frontend
+    | for public pages that don't require user authentication.
+    |
+    */
+    'token' => env('SYSTEM_API_TOKEN'),
+];
