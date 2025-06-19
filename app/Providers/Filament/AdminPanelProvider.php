d5877316228bcbe131cb92d16c2d95989704a2ba
Hannah Casier <hannah.casier@me.com>
2025-06-19 11:58:13 +0200

    fix image upload event

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -61,7 +61,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ])
-            ->viteTheme('resources/css/filament/admin/theme.css');
+            ]);
     }
 }
